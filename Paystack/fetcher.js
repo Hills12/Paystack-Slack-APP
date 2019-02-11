a144c381660961ffeab5d52a47b54ba9ab719eae
hills12 <tobiokedeji@gmail.com>
2019-02-11 13:55:03 +0100

    configured to work on heroku

--- Paystack/fetcher.js
+++ Paystack/fetcher.js
@@ -3,8 +3,11 @@ const puppeteer = require("puppeteer");
 exports.login = async (email = String, password = String, getWhat = String, teamID, submittedAmount, submittedEmail, submittedInstruction) => {
     try {
         const browser = await puppeteer.launch({headless: false,  args : [
-            '--window-size=1920,1080'
+            '--window-size=1920,1080', 
+            '--no-sandbox', 
+            '--disable-setuid-sandbox'
         ]});
+        
         const page = await browser.newPage();
 
         page.setViewport({ width: 1366, height: 768})
